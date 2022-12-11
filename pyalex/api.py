b6a741f0136d6b04376005646b455fabd8531ce2
Jonathan de Bruin <jonathandebruinos@gmail.com>
2022-12-11 09:43:10 +0100

    Add error handling for invalid queries (#3)

--- pyalex/api.py
+++ pyalex/api.py
@@ -44,6 +44,10 @@ def invert_abstract(inv_index):
         return " ".join(map(lambda x: x[0], sorted(l, key=lambda x: x[1])))
 
 
+class QueryError(ValueError):
+    pass
+
+
 class OpenAlexEntity(dict):
 
     pass
@@ -139,6 +143,20 @@ class BaseOpenAlex(object):
 
         return self.filter(openalex_id="|".join(record_list)).get()
 
+    def __getattr__(self, key):
+
+        if key == "groupby":
+            raise AttributeError(
+                "Object has no attribute 'groupby'. "
+                "Did you mean 'group_by'?")
+
+        if key == "filter_search":
+            raise AttributeError(
+                "Object has no attribute 'filter_search'. "
+                "Did you mean 'search_filter'?")
+
+        return getattr(self, key)
+
     def __getitem__(self, record_id):
 
         if isinstance(record_id, list):
@@ -177,8 +195,12 @@ class BaseOpenAlex(object):
         res = requests.get(
             url, headers={"User-Agent": "pyalex/" + __version__, "email": config.email}
         )
-        res.raise_for_status()
         res_json = res.json()
+
+        if res.status_code == 403 and "query parameters" in res_json["error"]:
+            raise QueryError(res_json["message"])
+
+        res.raise_for_status()
 
         # group-by or results page
         if "group-by" in self.params:
